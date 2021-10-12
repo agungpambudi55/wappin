4c8009cbe6831dc4c1d8e946ba08b337d4cf80ee
agungpambudi55 <agung.pambudi5595@gmail.com>
2021-10-12 14:39:14 +0700

    add send notification

--- app/Http/Controllers/WappinController.php
+++ app/Http/Controllers/WappinController.php
@@ -13,10 +13,10 @@ class WappinController extends Controller
 
         $arrResBody = json_decode($resBody->body(), true);
 
-        if($resBody->status() == 200){
+        if($arrResBody['status'] == 000){
             return response()->json(['success'=>true, 'message'=>'API Wappin (Whatsapp Integrator) works!'], $resBody->status());
         }else{
-            return response()->json(['success'=>false], $resBody->status());
+            return response()->json(['success'=>false], $arrResBody['status']);
         }
     }
 
@@ -30,26 +30,53 @@ class WappinController extends Controller
     public function getToken(Request $request)
     {
         $resBody = Http::withHeaders([
-            'Content-Type' => 'application/json',
-            'Authorization' => 'Basic '.base64_encode($request->client_id.':'.$request->secret_key),
-        ])
-        ->post(env('WAPPIN_URL').'/v1/token/get');
+                'Authorization' => 'Basic '.base64_encode($request->client_id.':'.$request->secret_key),
+                'Content-Type' => 'application/json'
+            ])
+            ->post(env('WAPPIN_URL').'/v1/token/get');
 
         $arrResBody = json_decode($resBody->body(), true);
+        $expiredDate = $arrResBody['data']['expired_datetime'];
+        $expired = (date('Y-m-d H:m:s')>=$expiredDate)?true:false;
 
         $data = array(
-            'token'=>$arrResBody['data']['access_token'],
-            'expired'=>$arrResBody['data']['expired_datetime']
-        );
+                'expired'=>$expired,
+                'expired_date'=>$expiredDate,
+                'token'=>$arrResBody['data']['access_token']
+            );
 
-        if($resBody->status() == 200){
-            return response()->json(['success'=>true, 'data'=>$data], $resBody->status());
+        if($arrResBody['status'] == 200){
+            return response()->json(['success'=>true, 'data'=>$data], $arrResBody['status']);
         }else{
-            return response()->json(['success'=>false], $resBody->status());
+            return response()->json(['success'=>false, 'message'=>$arrResBody['message']], $arrResBody['status']);
         }
     }
 
     public function sendNotification(Request $request)
     {
+        $token = $this->getToken($request)->getData()->data->token;        
+
+        $reqBody = array(
+                'client_id' => $request->client_id,
+                'project_id' => $request->project_id,
+                'type' => $request->type,
+                'recipient_number' => $request->recipient_number,
+                'params' => $request->params
+            );
+
+        $resBody = Http::withHeaders([
+                'Authorization' => 'Bearer '.$token,
+                'Content-Type' => 'application/json'
+            ])
+            ->post(env('WAPPIN_URL').'/v1/message/do-send-hsm', $reqBody);
+        
+        $arrResBody = json_decode($resBody->body(), true);
+
+        if($arrResBody['status'] == 200){
+            $msg = 'Notification has been sent successfully';
+            return response()->json(['success'=>true, 'message'=>$msg], $arrResBody['status']);
+        }else{
+            return response()->json(['success'=>false, 'message'=>$arrResBody['message']], $arrResBody['status']);
+        }
     }
 }
